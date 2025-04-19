c4b1e01f43f0484cab8e867e26515d0e7e6b1a32
mkhoatd <mkhoatd@gmail.com>
2025-04-19 10:51:40 +0700

    fix ui

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ ls
 mkdir -p iina-plugin-yt-subtitle
 bun build.js
 cp Info.json iina-plugin-yt-subtitle/Info.json
-cp -r ui iina-plugin-yt-subtitle/dist/ui
+cp -r ui iina-plugin-yt-subtitle/ui
 ./iina-plugin pack ./iina-plugin-yt-subtitle
