d2044b55df1b6ea8fdc507c468d452e37e9bf4bd
mkhoatd <mkhoatd@gmail.com>
2025-04-19 10:46:55 +0700

    increase versin

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+ls
 mkdir -p iina-plugin-yt-subtitle
 bun build.js
 cp Info.json iina-plugin-yt-subtitle/Info.json
 cp -r ui iina-plugin-yt-subtitle/dist/ui
-iina-plugin pack ./iina-plugin-yt-subtitle
+./iina-plugin pack ./iina-plugin-yt-subtitle
